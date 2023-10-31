9d9e7c1f89cbdc94f66074784081314c77c5ce21
Samet Çağrı Aktepe <sametcagriaktepe@gmail.com>
2023-10-31 16:30:18 +0300

    Initial Commit

---
+++ lazy-grids/lazy_gridsApp.swift
@@ -0,0 +1,17 @@
+//
+//  lazy_gridsApp.swift
+//  lazy-grids
+//
+//  Created by Samet Çağrı Aktepe on 31.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct lazy_gridsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

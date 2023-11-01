1cd3484d483e511192ac7577c939740980dd5441
Samet Çağrı Aktepe <sametcagriaktepe@gmail.com>
2023-11-01 09:58:43 +0300

    Multiple Column layout

--- lazy-grids/LayoutUIExampleView.swift
+++ lazy-grids/LayoutUIExampleView.swift
@@ -11,9 +11,16 @@ struct LayoutUIExampleView: View {
     @State private var isMultiColumn: Bool = false
 
     private var columns: [GridItem] {
-        [
-            GridItem(.flexible()),
-        ]
+        if isMultiColumn {
+            return [
+                GridItem(.flexible(minimum: 100)),
+                GridItem(.flexible(minimum: 100)),
+            ]
+        } else {
+            return [
+                GridItem(.flexible(minimum: 100)),
+            ]
+        }
     }
 
     var body: some View {
@@ -22,18 +29,19 @@ struct LayoutUIExampleView: View {
                 columns: columns,
                 alignment: .trailing,
                 spacing: 16
-            ) 
-            {
+            ) {
                 Button {
                     isMultiColumn.toggle()
                 } label: {
-                    Text("Toggle")
+                    Image(systemName: isMultiColumn ? "rectangle.grid.1x2" : "rectangle.grid.2x2")
+                        .font(.title)
+                        .foregroundColor(.white)
                         .padding()
-                        .frame(maxWidth: .infinity)
-                        .background(.pink)
+                        .background(.mint, in: RoundedRectangle(cornerRadius: 8, style: .continuous)
+                        )
                 }
-                
-                ForEach(1...50, id: \.self) { item in
+
+                ForEach(1 ... 50, id: \.self) { item in
                     Text("Item \(item)")
                         .frame(height: 250)
                         .frame(maxWidth: .infinity)
